1b76fba6296affa9088e0866390f1a154ac790c8
Matt Koscica <matt.koscica@gmail.com>
2017-04-20 14:09:06 +0200

    interesting.pl: add new stuff

--- interesting.pl
+++ interesting.pl
@@ -15,6 +15,7 @@ my %epochs = do {
     %$epochs;
 };
 
+say "opening progress socket...";
 my $progress_output_fh = IO::File->new("progress" => 'w');
 $progress_output_fh->autoflush(1);
 
@@ -25,17 +26,19 @@ until ( $epoch > ( $^T + ( 86400 * 100 ) ) ) {
     my $start_of_hour = $epoch - ( $epoch % 3600 );
     my $end_of_hour   = $start_of_hour + 3599;
 
+    dump_epoch($epoch, '1234567') if $epoch =~ m/1234567/;
+
     # lots of the same digit
     dump_epoch($epoch, 'repetition') if $epoch =~ m/([0-9])\1\1\1\1\1\1+/;
 
     # repeating doublets.
-    dump_epoch($epoch, 'doublets') if $epoch =~ m/([0-9]{2})\1\1\1\1/;
+    dump_epoch($epoch, 'doublets') if $epoch =~ m/([0-9]{2})\1\1\1/;
 
     # repeating triplets. sometime occur in a cluster, how do we detect this
     # and only post the only most interesting (the one that ends in 1)?
     dump_epoch($epoch, 'triplets') if $epoch =~ m/([0-9]{3})\1\1/; # and shift_epoch_by_x($epoch, 4) == $epoch;
 
-    # vertical symmetry, kinda boring.
+    # vertical symmetry, kinda boring, happens too often.
     # dump_epoch($epoch, 'vertical symmetry') if substr($epoch, 0, 5) == reverse(substr($epoch, 5, 5));
 
     $epoch++;
